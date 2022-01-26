655ced84858e8456af18933c13d5ee9ba1af5870
Danna <dannabraga@gmail.com>
2022-01-26 15:24:37 -0300

    first commit

---
+++ contagem.js
@@ -0,0 +1,13 @@
+let numeroAlunos = 10;
+for (let contador = 0; contador < numeroAlunos; contador++){
+
+if (contador == 0) {
+    console.log ("o número é 0")
+}
+
+     else if (contador % 2 == 0) {  
+     console.log (`o numero ${contador} é PAR`)
+     } else if (contador % 2 == 1) {
+        console.log (`o número ${contador} é IMPAR`)
+    }
+}
